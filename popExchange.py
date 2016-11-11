bb81649ae91b368081047e398595c953298c324b
dante <dfu3@njit.edu>
2016-11-11 16:14:44 -0500

    added currescies and grouping

--- popExchange.py
+++ popExchange.py
@@ -6,9 +6,19 @@ loadQ = ['EUR', 'USD', 'AUD', 'CHF', 'CAD', 'GBP', 'INR', 'JPY', 'MXN', 'RUB', '
 outerQ = loadQ[:]
 innerQ = loadQ[:]
 
-myDB = MySQLdb.connect(host="10.200.45.127",port=3306,user="dmz",passwd="letMe1n",db="user_info")
+myDB = MySQLdb.connect(host="10.200.173.26",port=3306,user="dmz",passwd="letMe1n",db="user_info")
 curs = myDB.cursor()
+qOut = str(curs.execute("select grouping from exchange order by id desc limit 1;"))
+currGroup = qOut[qOut.find("'")+1:1]
+currGroup = str(int(currGroup) + 1)
 curs.execute("truncate exchange;")
+curs.execute("truncate currencies;")
+#currGroup = 1 #uncomment if resetting
+print(currGroup)
+
+allCurrDesc = requests.get("https://openexchangerates.org/api/currencies.json").json()
+for curr in loadQ:
+    curs.execute("insert into currencies(currency, description)  values('" + curr + "', '" + allCurrDesc[curr] + "');")
 
 for outerCurr in outerQ:
     allCurr = requests.get("https://openexchangerates.org/api/latest.json?app_id=0e838b78b3234442a7628ddf4b3dec90&base=" + outerCurr).json()
@@ -18,8 +28,8 @@ for outerCurr in outerQ:
     for innerCurr in innerQ:
         if (innerCurr != outerCurr):
             print(outerCurr + ' ==> ' + innerCurr + ': ' + str(allRates[innerCurr]))
-            curs.execute("insert into exchange(currency_1, currency_2, rate)  values('" + outerCurr + "', '" + innerCurr + "', '" + str(allRates[innerCurr]) + "');")
-            curs.execute("insert into exchange_backup(currency_1, currency_2, rate)  values('" + outerCurr + "', '" + innerCurr + "', '" + str(allRates[innerCurr]) + "');")
+            curs.execute("insert into exchange(currency_1, currency_2, rate, grouping)  values('" + outerCurr + "', '" + innerCurr + "', '" + str(allRates[innerCurr]) + "', '" + str(currGroup) + "');")
+            curs.execute("insert into exchange_backup(currency_1, currency_2, rate, grouping)  values('" + outerCurr + "', '" + innerCurr + "', '" + str(allRates[innerCurr]) + "', '" + str(currGroup) + "');")
     innerQ.remove(outerCurr)
 
 myDB.commit()
