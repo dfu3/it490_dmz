9872f3c2cfa8295485a88ebadfcf1d08084d7499
dante <dfu3@njit.edu>
2016-12-14 11:11:08 -0500

    all delivs are met

--- justPop.py
+++ justPop.py
@@ -1,39 +1,19 @@
 import requests
-import os
 import json
 import MySQLdb
-import socket
-from getLimits import getLimits
 
 loadQ = ['EUR', 'USD', 'AUD', 'CHF', 'CAD', 'GBP', 'INR', 'JPY', 'MXN', 'RUB', 'CNY']
 outerQ = loadQ[:]
 innerQ = loadQ[:]
 
-DBIP = "10.200.173.68" #PAUL'S IP ADD
-DBIP = "10.200.173.193" #TIM'S IP ADD
-
-'''
-ret = os.system("ping -c 1 " + DBIP)
-if ret != 0:
-    DBIP = "10.200.173.193" #TIM'S IP ADD    
-print(ret)
-'''
-
-try:
-    myDB = MySQLdb.connect(host="10.200.173.68",port=3306,user="dmz",passwd="letMe1n",db="user_info")
-
-except(Exception):
-    myDB = MySQLdb.connect(host="10.200.173.193",port=3306,user="dmz",passwd="letMe1n",db="user_info")
-
+myDB = MySQLdb.connect(host="10.200.44.178",port=3306,user="dmz",passwd="letMe1n",db="user_info")
 curs = myDB.cursor()
 curs.execute("select grouping from exchange order by id desc limit 1;")
 currGroup = curs.fetchone()[0]
 currGroup = str(int(currGroup) + 1)
 curs.execute("truncate exchange;")
 curs.execute("truncate currencies;")
-curs.execute("truncate trade_limits;")
-
-#currGroup = 1 #uncomment if resetting 'grouping' column
+#currGroup = 1 #uncomment if resetting
 print(currGroup)
 
 allCurrDesc = requests.get("https://openexchangerates.org/api/currencies.json").json()
@@ -50,14 +30,6 @@ for outerCurr in outerQ:
             print(outerCurr + ' ==> ' + innerCurr + ': ' + str(allRates[innerCurr]))
             curs.execute("insert into exchange(currency_1, currency_2, rate, grouping)  values('" + outerCurr + "', '" + innerCurr + "', '" + str(allRates[innerCurr]) + "', '" + str(currGroup) + "');")
             curs.execute("insert into exchange_backup(currency_1, currency_2, rate, grouping)  values('" + outerCurr + "', '" + innerCurr + "', '" + str(allRates[innerCurr]) + "', '" + str(currGroup) + "');")
-
-            curs.execute( "select rate from exchange_backup where (currency_1='" + outerCurr + "' and currency_2='" + innerCurr + "') or (currency_1='" + innerCurr + "' and currency_2='" + outerCurr + "') order by grouping desc limit 30;" )
-
-            result = [item[0] for item in curs.fetchall()]
-            lims = getLimits(result)
-
-            curs.execute("insert into trade_limits(currency_1, currency_2, upper, lower, rate) values('" + outerCurr +  "', '" + innerCurr +  "', '" + str(lims[0]) + "', '" + str(lims[1]) + "', '" + str(allRates[innerCurr]) + "');")
-            
     innerQ.remove(outerCurr)
 
 myDB.commit()
